ba88ed37a7440b82443a839f43d7271dc0dbaf56
lbt666 <1319451895@qq.com>
2022-07-07 13:45:20 +0800

    2.0

---
+++ MThrid/MouseWheelEvent.js
@@ -0,0 +1,16 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var MThrid;
+(function (MThrid) {
+    var MouseWheelEvent = (function () {
+        function MouseWheelEvent() {
+        }
+        MouseWheelEvent.coordinateY = 0;
+        MouseWheelEvent.eventDispatcher = new egret.EventDispatcher();
+        return MouseWheelEvent;
+    }());
+    MThrid.MouseWheelEvent = MouseWheelEvent;
+    __reflect(MouseWheelEvent.prototype, "MThrid.MouseWheelEvent");
+})(MThrid || (MThrid = {}));
+//# sourceMappingURL=MouseWheelEvent.js.map
