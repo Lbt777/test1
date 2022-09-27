dac6322f6ca63644d32d13cac50510996d131309
lbt666 <1319451895@qq.com>
2022-09-27 18:01:38 +0800

    123131

--- static/config.js
+++ static/config.js
@@ -1,4 +1,8 @@
 export default {
-	// 后端配置
-	SERVER_API: "http://..."
+	estimateStartTime: "10:00", // 预约开始
+	estimateEndTime: "", // 预约结束
+	estimateReadonly: false, // 预约是否只读
+	jobStartTime: "10:00", // 营业开始
+	jobEndTime: "11:00", // 营业结束
+	jobReadonly: true, // 营业是否只读
 }
