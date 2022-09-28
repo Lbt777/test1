f29ab881148ff66e399adcd3cf178c6b746d1d9e
lbt666 <1319451895@qq.com>
2022-09-28 10:07:53 +0800

    123123

--- static/config.js
+++ static/config.js
@@ -10,13 +10,13 @@
 
 export default {
 	estimateStartTime: "10:00", // 预约开始
-	estimateEndTime: "11:00", // 预约结束
+	estimateEndTime: "", // 预约结束
 	estimateReadonly: false, // 预约只读不可修改
-	jobStartTime: "12:00", // 营业开始
-	jobEndTime: "21:00", // 营业结束
+	jobStartTime: "", // 营业开始
+	jobEndTime: "", // 营业结束
 	jobReadonly: false, // 营业只读不可修改
-	organizationcode: '9999', // 画室号
-	organizationcodeReadonly: true, // 画室号只读不可修改
-	abbreviation: '11111', // 画室编码
-	abbreviationReadonly: true // 画室编码只读不可修改
+	organizationcode: '', // 画室号
+	organizationcodeReadonly: false, // 画室号只读不可修改
+	abbreviation: '', // 画室编码
+	abbreviationReadonly: false // 画室编码只读不可修改
 }
